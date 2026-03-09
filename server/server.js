1ab9a04cd4418ee167c8dcc96590cdc9079bef38
ShivanshuSonwani <shivanshu.ed@gmail.com>
2026-03-09 11:51:04 +0530

    feat: Implement initial server setup with environment-based configuration and MongoDB integration.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import app from './src/app.js'
 import config from './src/config/config.js'
+import connectDB from './src/config/db.js'
 
 const startServer = () => {
     try {
+        connectDB()
+
         app.listen(config.port, () => {
             console.log(`Server is running on port ${config.port}`)
         })
--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 
 const config = {
     port: process.env.PORT || 3000,
+    MONGODB_URI: process.env.MONGODB_URI,
 }
 
 export default Object.freeze(config)
